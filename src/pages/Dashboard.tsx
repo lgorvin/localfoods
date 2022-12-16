b94b122b9cc38557bfe67132c0f3383f5fa8eb30
lgorvin <lgorvin78@gmail.com>
2022-12-16 14:54:40 +0000

    faff

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,10 +3,24 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 import { auth, db, logout } from "../firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import {
+  doc,
+  query,
+  collection,
+  getDocs,
+  where,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
+  const [newName, setNewName] = useState("");
+  const [docId, setDocId] = useState("");
+  const [changeName, setChangeName] = useState(false);
+  const handleClick = () => setChangeName(!changeName);
+  const [allUsers, setAllUsers] = useState("");
+  const [supplier, setSupplier] = useState(false);
   const navigate = useNavigate();
   const fetchUserName = async () => {
     try {
@@ -19,10 +33,41 @@ function Dashboard() {
       alert("An error occured while fetching user data");
     }
   };
+  const fetchUserName2 = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const cities: any = [];
+        querySnapshot.forEach((doc) => {
+          cities.push(doc.data().name);
+          setDocId(doc.id);
+          console.log(docId);
+          if (doc.data().supplier == true) {
+            setSupplier(true);
+          }
+        });
+
+        console.log("Users are: ", cities.join(", "));
+        setAllUsers(cities.join(", "));
+      });
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+
+  const nameChanger = async () => {
+    const nameRef = doc(collection(db, "users"), docId);
+    await updateDoc(nameRef, {
+      name: newName,
+    });
+  };
+
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
-    fetchUserName();
+    fetchUserName2();
   }, [user, loading]);
   return (
     <div className="dashboard">
@@ -30,6 +75,28 @@ function Dashboard() {
         Logged in as
         <div>{name}</div>
         <div>{user?.email}</div>
+        <div>
+          Name: {allUsers} <h1 onClick={handleClick}>Change?</h1>
+          {changeName && (
+            <>
+              <input
+                className="px-2"
+                type="text"
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+                placeholder="Full Name"
+              />
+              <button onClick={nameChanger} className="bg-green-500  px-2">
+                Submit
+              </button>
+            </>
+          )}
+        </div>
+        {supplier ? (
+          <h1>You have a supplier account</h1>
+        ) : (
+          <h1>You have a consumer account</h1>
+        )}
         <button className="dashboard__btn" onClick={logout}>
           Logout
         </button>
--- src/pages/SellFood.tsx
+++ src/pages/SellFood.tsx
@@ -1,9 +1,144 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import "./Dashboard.css";
+import { auth, db, logout } from "../firebase";
+import {
+  doc,
+  query,
+  collection,
+  getDocs,
+  where,
+  onSnapshot,
+  updateDoc,
+  addDoc,
+} from "firebase/firestore";
 
 const SellFood = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [docId, setDocId] = useState("");
+  const [uid, setUid] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  const [fetchDesc, setFetchDesc] = useState<
+    { title: string; desc: string; user: string; uid: string }[]
+  >([]);
+
+  const [fetchTitle, setFetchTitle] = useState([]);
+  // const [fetchDesc, setFetchDesc] = useState([]);
+
+  interface Post {
+    title: string;
+    desc: string;
+    user: string;
+    uid: string;
+  }
+
+  const postArr: Post[] = [];
+
+  const fetchUserName2 = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const cities: any = [];
+        querySnapshot.forEach((doc) => {
+          cities.push(doc.data().title);
+          setName(doc.data().name);
+          setUid(doc.data().uid);
+          setDocId(doc.id);
+        });
+      });
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+
+  const fetchPosts = async () => {
+    try {
+      const q = query(collection(db, "posts"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const titles: any = [];
+        const descs: any = [{}];
+        querySnapshot.forEach((doc) => {
+          titles.push(doc.data().title);
+          postArr.push({
+            title: doc.data().title,
+            desc: doc.data().desc,
+            user: doc.data().user,
+            uid: doc.data().uid,
+          });
+        });
+        //console.log(postArr);
+        //console.log("Post titles are: ", titles.join(", "));
+        setFetchTitle(titles);
+        setFetchDesc(postArr);
+        console.log(fetchDesc);
+      });
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+
+  const postMaker = async () => {
+    const docRef = await addDoc(collection(db, "posts"), {
+      title: title,
+      desc: desc,
+      user: name,
+      uid: uid,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/");
+    fetchUserName2();
+    fetchPosts();
+    for (var product of fetchDesc) {
+      console.log(product.title);
+      console.log(product.desc);
+    }
+  }, [user, loading]);
   return (
     <div>
       <div>Supplier Hub</div>
+      <div className="flex justify-center">
+        <input
+          className="px-2"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Food Item"
+        />
+        <input
+          className="px-2"
+          type="text"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+          placeholder="Description"
+        />
+        <button className="bg-green-500 px-2" onClick={postMaker}>
+          Create Post
+        </button>
+      </div>
+      <div>
+        <h1 className="text-center mt-10">Your Posts</h1>
+        <div>
+          {/* {fetchTitle.map((title) => (
+            <>
+              <h1 key={title}>title: {title}</h1>
+            </>
+          ))} */}
+        </div>
+      </div>
     </div>
   );
 };
